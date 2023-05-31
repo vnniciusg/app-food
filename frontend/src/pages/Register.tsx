8b0353ab4acc91a9a337afb3ea2fda6c706c468b
vnniciusg <90412694+vnniciusg@users.noreply.github.com>
2023-05-31 17:32:26 -0300

    Register setup

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Form from "../components/Form";
+
+const Register = () => {
+    return(
+        <div className="flex w-full h-screen">
+        <div className="w-full flex items-center justify-center h-auto">
+            <Form 
+                title="Bem vindo!"
+                subtitle="Subtitulo"
+             />
+        </div>
+     </div>
+    )
+}
+
+
+export default Register;
